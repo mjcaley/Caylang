d62ddacf75514f33d48ee9e8a8a7107451aab800
Michael Caley <mjcaley@darkarctic.com>
2020-03-14 14:20:02 -0400

    Add solution and initial assembler project

---
+++ Assembler/Position.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Caylang.Assembler
+{
+    public struct Position
+    {
+        public Position(int line, int column)
+        {
+            Line = line;
+            Column = column;
+        }
+
+        public int Line { get; }
+
+        public int Column { get; }
+    }
+}
---
+++ Assembler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Caylang.Assembler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
