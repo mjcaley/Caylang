93578bfb63b983f0291f5b151f7007c5dde99a92
Michael Caley <mjcaley@darkarctic.com>
2020-04-02 15:17:41 -0400

    Added parser tests with empty arrays

--- Assembler.Tests/ParserTests.cs
+++ Assembler.Tests/ParserTests.cs
@@ -36,6 +36,15 @@ namespace CayLang.Assembler.Tests
 
             Assert.Throws<UnexpectedTokenException>(() => p.Halt());
         }
+
+        [Fact]
+        public void HaltFailNull()
+        {
+            var p = new Parser(System.Array.Empty<Token>());
+
+            Assert.Throws<UnexpectedTokenException>(() => p.Halt());
+        }
+
         [Fact]
         public void PopSuccess()
         {
@@ -65,6 +74,54 @@ namespace CayLang.Assembler.Tests
             );
 
             Assert.Throws<UnexpectedTokenException>(() => p.Pop());
+        }
+
+        [Fact]
+        public void PopFailNull()
+        {
+            var p = new Parser(System.Array.Empty<Token>());
+
+            Assert.Throws<UnexpectedTokenException>(() => p.Pop());
+        }
+
+        [Fact]
+        public void AddSuccess()
+        {
+            var p = new Parser(
+                new[]
+                {
+                    new Token(TokenType.Add, 1),
+                    new Token(TokenType.i8Type, 1)
+                }
+            );
+            var n = p.Add();
+
+            Assert.Equal(Caylang.Assembler.ParseTree.Type.Integer8, n.ReturnType);
+            Assert.Equal(Caylang.Assembler.ParseTree.InstructionType.Add, n.Instruction);
+            Assert.Equal(1, n.Line);
+
+            Assert.Null(p.Current);
+        }
+
+        [Fact]
+        public void AddFailUnexpectedToken()
+        {
+            var p = new Parser(
+                new[]
+                {
+                    new Token(TokenType.EndOfFile, 1)
+                }
+            );
+
+            Assert.Throws<UnexpectedTokenException>(() => p.Add());
+        }
+
+        [Fact]
+        public void AddFailNull()
+        {
+            var p = new Parser(System.Array.Empty<Token>());
+
+            Assert.Throws<UnexpectedTokenException>(() => p.Add());
         }
     }
 }
