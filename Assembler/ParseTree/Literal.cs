32b394a9e56121f22a47fb4927c1e99b17aeb8c9
Michael Caley <mjcaley@darkarctic.com>
2020-04-12 14:27:56 -0400

    Removed redundant parse tree literals

--- Assembler/ParseTree/Literal.cs
+++ Assembler/ParseTree/Literal.cs
@@ -6,31 +6,6 @@ namespace Caylang.Assembler.ParseTree
 {
 	public abstract class Literal { }
 
-	#region AST literals
-
-	public class ASTLiteral : Literal
-	{
-		public ASTLiteral(string value) => Value = value;
-
-		public string Value { get; }
-	}
-
-	public class IntegerLiteral : ASTLiteral
-	{
-		public IntegerLiteral(string value) : base(value) { }
-	}
-
-	public class FloatLiteral : ASTLiteral
-	{
-		public FloatLiteral(string value) : base(value) { }
-	}
-	#endregion
-
-	public class VoidLiteral : Literal
-	{
-		public VoidLiteral() { }
-	}
-
 	public class ValueLiteral<T> : Literal
 	{
 		public ValueLiteral(T value) => Value = value;
@@ -38,54 +13,14 @@ namespace Caylang.Assembler.ParseTree
 		public T Value { get; }
 	}
 
-	public class Int8Literal : ValueLiteral<sbyte>
+	public class IntegerLiteral : ValueLiteral<string>
 	{
-		public Int8Literal(sbyte value) : base(value) { }
+		public IntegerLiteral(string value) : base(value) { }
 	}
 
-	public class UInt8Literal : ValueLiteral<byte>
+	public class FloatLiteral : ValueLiteral<string>
 	{
-		public UInt8Literal(byte value) : base(value) { }
-	}
-
-	public class Int16Literal : ValueLiteral<short>
-	{
-		public Int16Literal(short value) : base(value) { }
-	}
-
-	public class UInt16Literal : ValueLiteral<ushort>
-	{
-		public UInt16Literal(ushort value) : base(value) { }
-	}
-
-	public class Int32Literal : ValueLiteral<int>
-	{
-		public Int32Literal(int value) : base(value) { }
-	}
-
-	public class UInt32Literal : ValueLiteral<uint>
-	{
-		public UInt32Literal(uint value) : base(value) { }
-	}
-
-	public class Int64Literal : ValueLiteral<long>
-	{
-		public Int64Literal(long value) : base(value) { }
-	}
-
-	public class UInt64Literal : ValueLiteral<ulong>
-	{
-		public UInt64Literal(ulong value) : base(value) { }
-	}
-
-	public class Float32Literal : ValueLiteral<float>
-	{
-		public Float32Literal(float value) : base(value) { }
-	}
-
-	public class Float64Literal : ValueLiteral<double>
-	{
-		public Float64Literal(double value) : base(value) { }
+		public FloatLiteral(string value) : base(value) { }
 	}
 
 	public class StringLiteral : ValueLiteral<string>
