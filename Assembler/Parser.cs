fcaa10c3d513d69c6b851af3e96bc04fc1660d83
Michael Caley <mjcaley@darkarctic.com>
2020-04-12 14:27:07 -0400

    Moved Assembler exceptions into its own file

--- Assembler/Parser.cs
+++ Assembler/Parser.cs
@@ -1,47 +1,10 @@
 ﻿using Caylang.Assembler.ParseTree;
-using Caylang.Assembler;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using InstructionType = Caylang.Assembler.ParseTree.InstructionType;
 
 namespace Caylang.Assembler
 {
-    public class ParserException : Exception
-    {
-        public ParserException()
-        {
-        }
-
-        public ParserException(string message) : base(message)
-        {
-        }
-
-        public ParserException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
-    }
-
-    public class UnexpectedTokenException : ParserException
-    {
-        public UnexpectedTokenException(Token? found) : base(_message) => FoundToken = found;
-
-        public UnexpectedTokenException(Token? found, Exception innerException) : base(_message, innerException) => FoundToken = found;
-
-        private const string _message = "Unexpected token";
-
-        public Token? FoundToken { get; }
-    }
-
-    public class IntegerConversionException : ParserException
-    {
-        public IntegerConversionException() => At = null;
-        
-        public IntegerConversionException(Token? token) => At = token;
-
-        public Token? At { get; }
-    }
-
     public class Parser
 	{
         private readonly IEnumerator<Token> tokens;
