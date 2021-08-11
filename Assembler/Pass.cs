461f95df4f94414559915bacee562711d7cd3f2b
Michael Caley <mjcaley@darkarctic.com>
2021-08-11 08:01:49 -0400

    Added parse tree pass

---
+++ Assembler/Pass.cs
@@ -0,0 +1,23 @@
+﻿namespace Caylang.Assembler
+{
+	public class Pass
+	{
+		public virtual void Visit(ParseTreeBranch node)
+		{
+			foreach (var child in node.Children)
+			{
+				switch (child)
+				{
+					case ParseTreeBranch branch:
+						Visit(branch);
+						break;
+					case ParseTreeLeaf leaf:
+						Visit(leaf);
+						break;
+				};
+			}
+		}
+
+		public virtual void Visit(ParseTreeLeaf node) { }
+	}
+}
